10abee4cfccb97c6b7f1f3bc5b56afc34384d0b1
Jochem van Kranenburg <43204141+JVKran@users.noreply.github.com>
2019-01-24 17:17:54 +0100

    Update ProMiniSerialToWemos.ino

--- ProMiniSerialToWemos.ino
+++ ProMiniSerialToWemos.ino
@@ -1,27 +1,69 @@
+#include <Wire.h>
+#include "MAX30100_PulseOximeter.h"
+
+#define REPORTING_PERIOD_MS     30000
 unsigned long startPress;
 
-void setup() {
-  pinMode(11, INPUT_PULLUP);
-  Serial.begin(115200);
+PulseOximeter pox;
+
+uint32_t tsLastReport = 0;
+
+void onBeatDetected()
+{
+   // Serial.println("Beat!");
 }
 
-void rotaryPushButton(){
-  if (digitalRead(11) == LOW){
-    startPress = millis();
-    while (digitalRead(11) == LOW){
-      delay(10);
-    }
-    if (millis() - startPress > 500){
-      Serial.println(" longPress");
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(7, INPUT_PULLUP);
+    Serial.print("Initializing pulse oximeter..");
+
+    // Initialize the PulseOximeter instance
+    // Failures are generally due to an improper I2C wiring, missing power supply
+    // or wrong target chip
+    if (!pox.begin()) {
+        Serial.println("FAILED");
+        for(;;);
     } else {
-      Serial.println(" shortPress");
+        Serial.println("SUCCESS");
     }
-    startPress = 0;
+
+    // The default current for the IR LED is 50mA and it could be changed
+    //   by uncommenting the following line. Check MAX30100_Registers.h for all the
+    //   available options.
+    // pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+
+    // Register a callback for the beat detection
+    pox.setOnBeatDetectedCallback(onBeatDetected);
+}
+
+void loop()
+{
+    // Make sure to call update as fast as possible
+    pox.update();
+
+    // Asynchronously dump heart rate and oxidation levels to the serial
+    // For both, a value of 0 means "invalid"
+    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
+        Serial.println("Heart rate:" + String(pox.getHeartRate()) + "bpm / SpO:" + String(pox.getSpO2()) + "%");
+        tsLastReport = millis();
+    }
+    if (digitalRead(7) == LOW){
+      pox.shutdown();
+      startPress = millis();
+      while (digitalRead(7) == LOW){
+        delay(10);
+      }
+      if (millis() - startPress > 500){
+        Serial.println(" 2");
+        Serial.println("");
+      } else {
+        Serial.println(" 1");
+        Serial.println("");
+        delay(500);
+      }
+      pox.resume();
+      startPress = 0;
   }
 }
-
-void loop() {
-  rotaryPushButton();
-}
-
-
